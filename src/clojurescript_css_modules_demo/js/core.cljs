9cbfc87167020a270badd89a57c0daf6fb49ffd7
dhruvbhatia <dhruv@dhruvbhatia.com>
2015-11-14 10:50:34 -0500

    Refactored and improved get-css fn to handle nested hiccup structures

--- src/clojurescript_css_modules_demo/js/core.cljs
+++ src/clojurescript_css_modules_demo/js/core.cljs
@@ -3,25 +3,33 @@
             [reagent.core :as r]
             [clojure.string :as string]
             [cljs.reader :as reader]
+            [clojure.walk :refer [prewalk]]
             ))
 
 (enable-console-print!)
 
 (println "CSS Modules:" js/cssModules)
 
+(defn ^:private populate-css-module-classes [e]
+  (prewalk (fn [x]
+             (let [key (if (keyword? x) (str (name x)))
+                   class-lookup (if (some? key) (re-find #"(?:CSS>)[^\s][a-zA-Z>-]+" key))
+                   tree (if (some? key) (rest (string/split class-lookup #">")))
+                   class-names (or (if (not (empty? tree)) (apply aget (js* "cssModules") tree)) ".")
+                   joined-class-names (string/replace-all class-names #" " ".")
+                   new-key (if (some? key) (keyword (string/replace-first key #"(?:CSS>)[^\s][a-zA-Z>-]+" joined-class-names)))
+                   ]
+
+               (if (some? key) new-key x)
+               )
+             ) e)
+  )
+
 (defn get-css
   "Helper fn that takes Reagent's hiccup syntax and replaces .CSS>x classes with their respective CSS module name(s). See usage in header below."
-  [i]
-  (let [stringified (str i)
-        class-lookup (re-find #"(?:CSS>)[^\s][a-zA-Z>-]+" stringified)
-        tree (rest (string/split class-lookup #">"))
-        class-names (or (apply aget (js* "cssModules") tree) ".")
-        joined-class-names (string/replace-all class-names #" " ".")
-        output (reader/read-string (string/replace-first stringified #"(?:CSS>)[^\s][a-zA-Z>-]+" joined-class-names))
-        finished? (nil? (re-find #"(?:CSS>)[^\s][a-zA-Z>-]+" (str output)))]
-    (if finished?
-      output
-      (recur output))))
+  [v]
+  (into [] (map #(populate-css-module-classes %) v))
+  )
 
 (defn header []
   (get-css
